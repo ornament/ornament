155b44070fdef2ef849d3f10683c58e52dca5956
Christian Tegnér <christian.tegner@gmail.com>
2014-02-01 22:08:43 +0100

    Correct jshintrc filename + glob patterns

--- test/basics.js
+++ test/basics.js
@@ -3,7 +3,7 @@ var test = require('tape');
 var _ = require('lodash');
 var template = require('../');
 
-test('basic template compilation', function (t) {
+test('basic template compilation', function(t) {
     t.plan(1);
 
     var compiled = template(fs.readFileSync('test/templates/empty.t', 'UTF-8'));
--- test/input.js
+++ test/input.js
@@ -1,18 +1,17 @@
 var fs = require('fs');
 var jsdom = require('jsdom').jsdom;
 var test = require('tape');
-var _ = require('lodash');
 var template = require('../');
 
-test('basic markup parsing', function (t) {
+test('basic markup parsing', function(t) {
     t.plan(4);
 
     template.set({
-        document: jsdom("")
+        document: jsdom('')
     });
     var compiled = template(fs.readFileSync('test/templates/input.t', 'UTF-8'));
     t.equal(compiled.tree.length, 1, 'should parse a single element');
-    t.equal(compiled.tree[0].tagName.toLowerCase(), "input", 'with the correct tag name');
-    t.equal(compiled.tree[0].getAttribute("type"), "number", 'with the correct type attribute');
-    t.equal(compiled.tree[0].getAttribute("pattern"), "[0-9]", 'with the correct pattern attribute');
+    t.equal(compiled.tree[0].tagName.toLowerCase(), 'input', 'with the correct tag name');
+    t.equal(compiled.tree[0].getAttribute('type'), 'number', 'with the correct type attribute');
+    t.equal(compiled.tree[0].getAttribute('pattern'), '[0-9]', 'with the correct pattern attribute');
 });
