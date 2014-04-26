bdc0c4d153fef6a7562187d541a8a892f0e29447
Christian Tegnér <christian.tegner@gmail.com>
2014-04-26 22:10:23 +0200

    Convert 007-multiple-elements.js to compiler/runtime

--- test/007-multiple-elements/test.js
+++ test/007-multiple-elements/test.js
@@ -1,34 +1,38 @@
 var fs = require('fs');
 var jsdom = require('jsdom').jsdom;
 var test = require('tape');
-var template = require('../');
+var compiler = require('../../compiler.js');
+var runtime = require('../../runtime.js');
 
 test('parsing of multiple top-level elements', function(t) {
-    t.plan(19);
+    t.plan(20);
 
-    template.set({
+    var compiled = compiler(fs.readFileSync('test/007-multiple-elements/multiple-elements.t', 'UTF-8'));
+    t.deepEqual(compiled, require('./compiled.json'));
+
+    runtime.set({
         document: jsdom('')
     });
-    var compiled = template(fs.readFileSync('test/templates/multiple-elements.t', 'UTF-8'));
-    t.equal(compiled.tree.length, 5, 'should parse all the top-level elements');
-    var el = compiled.tree[0];
+    var tree = runtime(compiled);
+    t.equal(tree.childNodes.length, 5, 'should parse all the top-level elements');
+    var el = tree.childNodes[0];
     t.equal(el.nodeName.toLowerCase(), 'label');
     t.equal(el.getAttribute('for'), 'foo');
     t.equal(el.childNodes.length, 1);
     t.equal(el.childNodes[0].nodeName.toLowerCase(), '#text');
     t.equal(el.childNodes[0].nodeValue, 'Number');
-    el = compiled.tree[1];
+    el = tree.childNodes[1];
     t.equal(el.nodeName.toLowerCase(), '#text');
     t.equal(el.nodeValue, '\n');
-    el = compiled.tree[2];
+    el = tree.childNodes[2];
     t.equal(el.nodeName.toLowerCase(), 'input');
     t.equal(el.getAttribute('id'), 'foo');
     t.equal(el.getAttribute('type'), 'number');
     t.equal(el.getAttribute('pattern'), '[0-9]');
-    el = compiled.tree[3];
+    el = tree.childNodes[3];
     t.equal(el.nodeName.toLowerCase(), '#text');
     t.equal(el.nodeValue, '\n');
-    el = compiled.tree[4];
+    el = tree.childNodes[4];
     t.equal(el.nodeName.toLowerCase(), 'button');
     t.equal(el.getAttribute('class'), 'btn btn-primary');
     t.equal(el.childNodes.length, 1);
