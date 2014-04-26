4b912df22c6769fecc698fb7b54e6b7075e4175c
Christian Tegnér <christian.tegner@gmail.com>
2014-04-26 21:36:01 +0200

    Convert 005-text.js to compiler/runtime

---
+++ test/005-text/test.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var jsdom = require('jsdom').jsdom;
+var test = require('tape');
+var compiler = require('../../compiler.js');
+var runtime = require('../../runtime.js');
+
+test('basic text node parsing', function(t) {
+    t.plan(4);
+
+    var compiled = compiler(fs.readFileSync('test/005-text/text.t', 'UTF-8'));
+    t.deepEqual(compiled, require('./compiled.json'));
+
+    runtime.set({
+        document: jsdom('')
+    });
+    var tree = runtime(compiled);
+    t.equal(tree.childNodes.length, 1);
+    t.equal(tree.childNodes[0].nodeName.toLocaleLowerCase(), '#text');
+    t.equal(tree.childNodes[0].nodeValue, 'Hello World!\n');
+});
