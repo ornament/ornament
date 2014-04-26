d671015d8d5e7bd0bb91f0f3c4b3f7cd38c67725
Christian Tegnér <christian.tegner@gmail.com>
2014-04-26 22:42:57 +0200

    Convert 002-re-render-on-attribute-change.js to compiler/runtime

--- test/009-react-to-changes/test.js
+++ test/009-react-to-changes/test.js
@@ -1,19 +1,23 @@
-var _ = require('lodash');
+var fs = require('fs');
 var jsdom = require('jsdom').jsdom;
 var test = require('tape');
+var _ = require('lodash');
+var compiler = require('../../compiler.js');
 var runtime = require('../../runtime.js');
 var Model = require('backbone').Model;
 
 test('reactive templates', function(t) {
-    t.plan(5);
+    t.plan(6);
+
+    var compiled = compiler(fs.readFileSync('test/009-react-to-changes/interpolation.t', 'UTF-8'));
+    t.deepEqual(compiled, require('./compiled.json'));
 
     runtime.set({
         document: jsdom(''),
         binding: require('../../binding-backbone.js')
     });
-    var template = require('../compiled/basic.json');
-    var model = new Model();
-    var tree = runtime(template, model);
+    var data = new Model();
+    var tree = runtime(compiled, data);
 
     function html() {
         return _.reduce(tree.childNodes, function(str, child) {
@@ -23,22 +27,22 @@ test('reactive templates', function(t) {
 
     t.equal(html(), '<span>, </span>\n<span>Hello  !</span>\n<span>Username: </span>\n<span>\'\'</span>');
 
-    model.set('first', 'Drake');
+    data.set('first', 'Drake');
 
     t.equal(html(), '<span>, Drake</span>\n<span>Hello Drake !</span>\n' +
         '<span>Username: Drake</span>\n<span>\'Drake\'</span>');
 
-    model.set('last', 'Bell');
+    data.set('last', 'Bell');
 
     t.equal(html(), '<span>Bell, Drake</span>\n<span>Hello Drake Bell!</span>\n' +
         '<span>Username: DrakeBell</span>\n<span>\'Drake\'</span>');
 
-    model.unset('first');
+    data.unset('first');
 
     t.equal(html(), '<span>Bell, </span>\n<span>Hello  Bell!</span>\n' +
         '<span>Username: Bell</span>\n<span>\'\'</span>');
 
-    model.clear();
+    data.clear();
 
     t.equal(html(), '<span>, </span>\n<span>Hello  !</span>\n' +
         '<span>Username: </span>\n<span>\'\'</span>');
