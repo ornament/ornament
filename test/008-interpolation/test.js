9a5fdc4b5dd1d8fb13188c9b0c06faa76294cb31
Christian Tegnér <christian.tegner@gmail.com>
2014-04-26 22:32:51 +0200

    Convert 008-interpolation to compiler/runtime

---
+++ test/008-interpolation/test.js
@@ -0,0 +1,51 @@
+var fs = require('fs');
+var jsdom = require('jsdom').jsdom;
+var test = require('tape');
+var compiler = require('../../compiler.js');
+var runtime = require('../../runtime.js');
+
+test('basic string interpolation', function(t) {
+    t.plan(24);
+
+    var compiled = compiler(fs.readFileSync('test/008-interpolation/interpolation.t', 'UTF-8'));
+    t.deepEqual(compiled, require('./compiled.json'));
+
+    runtime.set({
+        document: jsdom('')
+    });
+    var data = {
+        first: 'John',
+        last: 'Doe'
+    };
+    var tree = runtime(compiled, data);
+    t.equal(tree.childNodes.length, 7);
+    var el = tree.childNodes[0];
+    t.equal(el.nodeName.toLowerCase(), 'span');
+    t.equal(el.childNodes.length, 1);
+    t.equal(el.childNodes[0].nodeName.toLowerCase(), '#text');
+    t.equal(el.childNodes[0].nodeValue, 'Doe, John');
+    el = tree.childNodes[1];
+    t.equal(el.nodeName.toLowerCase(), '#text');
+    t.equal(el.nodeValue, '\n');
+    el = tree.childNodes[2];
+    t.equal(el.nodeName.toLowerCase(), 'span');
+    t.equal(el.childNodes.length, 1);
+    t.equal(el.childNodes[0].nodeName.toLowerCase(), '#text');
+    t.equal(el.childNodes[0].nodeValue, 'Hello John Doe!');
+    el = tree.childNodes[3];
+    t.equal(el.nodeName.toLowerCase(), '#text');
+    t.equal(el.nodeValue, '\n');
+    el = tree.childNodes[4];
+    t.equal(el.nodeName.toLowerCase(), 'span');
+    t.equal(el.childNodes.length, 1);
+    t.equal(el.childNodes[0].nodeName.toLowerCase(), '#text');
+    t.equal(el.childNodes[0].nodeValue, 'Username: JohnDoe');
+    el = tree.childNodes[5];
+    t.equal(el.nodeName.toLowerCase(), '#text');
+    t.equal(el.nodeValue, '\n');
+    el = tree.childNodes[6];
+    t.equal(el.nodeName.toLowerCase(), 'span');
+    t.equal(el.childNodes.length, 1);
+    t.equal(el.childNodes[0].nodeName.toLowerCase(), '#text');
+    t.equal(el.childNodes[0].nodeValue, '\'John\'');
+});
