7d5ffe453895c11e6a78fccf4d32d61e6a11bef5
Christian Tegnér <christian.tegner@gmail.com>
2014-07-30 21:27:10 +0200

    Correct a test suit description

--- test/018-html-entities/test.js
+++ test/018-html-entities/test.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var test = require('tape');
 var compiler = require('../../compiler.js');
 
-test('parses and renders comments', function(t) {
+test('compiler converts HTML entities into corresponding unicode characters', function(t) {
     t.plan(1);
 
     var compiled = compiler(fs.readFileSync(__dirname + '/entities.t', 'UTF-8'));
